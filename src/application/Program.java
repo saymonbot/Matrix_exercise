d00a3af8456f27d702650b9b4f988c7d0af0d50f
saymonbot <saymon_botelho123@outlook.com>
2024-02-24 13:56:14 -0300

    Matrix Exercise

--- src/application/Program.java
+++ src/application/Program.java
@@ -10,20 +10,30 @@ public class Program {
 		
 		System.out.println("Matrix order: ");
 		int n = scan.nextInt();
-		
+	
 		int[][] mat = new int[n][n];
+		
+		int neg = 0;
 		
 		for(int i = 0; i < n; i++) {
 			for(int j = 0; j < n; j++) {
+				
 				System.out.println("Insira o valor da posição " + i + "" + j);
 				mat[i][j] = scan.nextInt();
+				
+				if(mat[i][j] < 0) {
+					neg++;
+				}
 			}
 		}
 		
-
+		System.out.println("Main Diagonal: ");
+		for(int i = 0; i < n; i++) {
+			System.out.println(mat[i][i] + " ");
+		}
+		
+		System.out.println("Negatives Numbers: " + neg);
 		
 		scan.close();
-
 	}
-
 }
